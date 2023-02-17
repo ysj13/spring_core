efc5e2373cc8dce1cead561ee08aae612e700838
sj <ysj13@kakao.com>
2023-02-18 01:15:28 +0900

    spring core e02 SOLID 원칙(SRP, OCP, DIP) 적용하기

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,33 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+// AppConfig 는 애플리케이션의 실제 동작에 필요한 구현객체를 생성하는 클래스임.
+// AppConfig 는 생성한 객체 인스턴스의 레퍼런스를 생성자를 통해서 주입(연결) 해준다
+public class AppConfig {
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+
+}
